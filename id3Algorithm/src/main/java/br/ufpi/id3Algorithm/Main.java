0106e247cd63bec3a68fe2f742fc6b6a2bfb40bd
Ronyerison <ronyerisonbraga@gmail.com>
2015-11-13 15:33:21 -0300

    Commit Arquivos iniciais.

---
+++ id3Algorithm/src/main/java/br/ufpi/id3Algorithm/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.ufpi.id3Algorithm;
+
+/**
+ * @author Ronyerison
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ id3Algorithm/src/test/java/id3Algorithm/Teste.java
@@ -0,0 +1,5 @@
+package id3Algorithm;
+
+public class Teste {
+
+}
