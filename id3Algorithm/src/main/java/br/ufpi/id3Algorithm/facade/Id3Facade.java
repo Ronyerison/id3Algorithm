9eee86a871cb868558e782177c7272b31eff7fc7
Ronyerison <ronyerisonbraga@gmail.com>
2015-11-23 12:14:45 -0300

    Inclusão do método para classificação na fachada.

--- id3Algorithm/src/main/java/br/ufpi/id3Algorithm/facade/Id3Facade.java
+++ id3Algorithm/src/main/java/br/ufpi/id3Algorithm/facade/Id3Facade.java
@@ -14,14 +14,19 @@ import br.ufpi.id3Algorithm.model.tree.Node;
  */
 public class Id3Facade {
 	private Id3Algorithm id3Algorithm;
+	private Node<String> tree;
 	
 	public Id3Facade() {
 		this.id3Algorithm = new Id3Algorithm();
 	}
 
-
+	/**
+	 * @param trainningSet
+	 * @return
+	 */
 	public Node<String> buildTree(List<String[]> trainningSet){
-		return id3Algorithm.buildTree(trainningSet);
+		this.tree = buildTree(trainningSet);
+		return this.tree;
 	}
 	
 	/**
@@ -37,6 +42,31 @@ public class Id3Facade {
 	 */
 	public void setId3Algorithm(Id3Algorithm id3Algorithm) {
 		this.id3Algorithm = id3Algorithm;
+	}
+
+	/**
+	 * @return the tree
+	 */
+	public Node<String> getTree() {
+		return tree;
+	}
+
+	/**
+	 * @param tree the tree to set
+	 */
+	public void setTree(Node<String> tree) {
+		this.tree = tree;
+	}
+	
+	/**
+	 * @param testSet
+	 * @return
+	 */
+	public List<String> classifyTestSet(List<String[]> testSet){
+		if(this.tree != null){
+			return id3Algorithm.classificationTestSet(testSet, tree);
+		}
+		return null;
 	}
 	
 }
